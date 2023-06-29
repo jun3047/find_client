3e116e4129f087056b96b13ad29edcd4e16592e8
jeong jun <jungjunjenon3047@gmail.com>
2023-06-30 03:17:39 +0900

    login 화면전환 끝

---
+++ web/src/App.tsx
@@ -0,0 +1,11 @@
+import { Routes, Route } from 'react-router-dom';
+import Login from './page/Login';
+
+function App() {
+  return (
+  <Routes>
+      <Route path="/" element={<Login />} />
+  </Routes>)
+}
+
+export default App;
