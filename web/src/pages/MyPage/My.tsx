f43c2063abbdc9d50a5e096cf1ec18e45411152d
jeong jun <jungjunjenon3047@gmail.com>
2023-07-02 18:56:48 +0900

    my page

--- web/src/pages/MyPage/My.tsx
+++ web/src/pages/MyPage/My.tsx
@@ -1,5 +1,96 @@
+import styled from "@emotion/styled";
+import { Post } from "../../components/Post";
+import { AlignBox, EmtpyBox, MarginBox, Text } from "../../styles/atom";
+
 const My = () => {
-    return <h1>My</h1>
+
+
+    return (<>
+        <AlignBox align="center" justify="top">
+            <EmtpyBox height={10} />
+            <Profile />
+            <EmtpyBox height={5} />
+            <Text
+                content="My"
+                fontsize={20}
+            />
+            <EmtpyBox height={1} />
+            <Text
+                content="@id"
+                fontsize={12}
+                color="#707070"
+            />
+            <EmtpyBox height={10} />
+            <AlignBox direction="row" style={{}}>
+                <FeedInfo num={1} title="글"/>
+                <FeedInfo num={1} title="파인드"/>
+            </AlignBox>
+            <EmtpyBox height={10} />
+            <PostInfo />
+            <EmtpyBox height={10} />
+            <Post question={"질문"} content={"내용"} writer={"id"} />
+        </AlignBox>
+    </>)
 }
 
+const Profile = () => {
+    return (
+        <AlignBox justify="top" align="center">
+            <EmtpyBox width={10} />
+            <ProfileCircle />
+            <EmtpyBox width={10} />
+        </AlignBox>
+    )
+}
+
+const ProfileCircle = styled.div`
+    width: 50px;
+    height: 50px;
+    border-radius: 50%;
+    background-color: #EF8080;
+`
+
+
+const FeedInfo = ({num, title}:{
+    num: number,
+    title: string
+}) => {
+    
+    return (
+        <AlignBox justify="top" align="center">
+            <Text
+                content={num.toString()}
+                fontsize={12}
+                color="#000"
+                weight={700}
+            />
+            <EmtpyBox width={10} />
+            <Text
+                content={title}
+                fontsize={12}
+                color="#707070"
+            />
+            <EmtpyBox width={1} />
+        </AlignBox>
+    )
+}
+
+const PostInfo = () => {
+    
+    return (
+        <AlignBox align="center" style={{borderBottom:'1px solid rgba(0, 0, 0, 0.25)'}}>
+            <MarginBox bottom={2}>
+                <Text
+                    content="내가 쓴 글"
+                    fontsize={16}
+                    color="#000"
+                    weight={700}
+                />
+            </MarginBox>
+        </AlignBox>
+    )
+}
+
+
 export default My;
+
