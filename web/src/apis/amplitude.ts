9ada29a5cbb4533a13d34221111a91d2070f4e25
jeong jun <jungjunjenon3047@gmail.com>
2023-07-19 10:14:39 +0900

    amplitude init

---
+++ web/src/apis/amplitude.ts
@@ -0,0 +1,37 @@
+import * as Sentry from '@sentry/react';
+import * as amplitude from 'amplitude-js';
+import memoize from 'lodash/memoize';
+
+const init = memoize(() => {
+    const apiKey = process.env.AMPLITUDE_API_KEY;
+    if (!apiKey) return;
+
+    amplitude.getInstance().init(apiKey, undefined, {
+        includeReferrer: true,
+        disableCookies: true,
+    })
+})
+
+
+export const trackEvent = (eventName: string, data?: Record<string, any>) => {
+    if(process.env.NODE_ENV !== 'production') return;
+
+    init();
+
+    amplitude.getInstance().logEvent(eventName, data);
+}
+
+// trackEvent(트랙 이벤트 )
+
+export const setUserForTracking = (userId: string, name?: string) => {
+    if (process.env.NODE_ENV !== 'production') return;
+
+    Sentry.configureScope((scope:any) => {
+        scope.setUser({ userId })
+    })
+
+    init();
+
+    amplitude.getInstance().setUserId(userId);
+    amplitude.getInstance().setUserProperties({ name });
+}
